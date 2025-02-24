d3616f8a2d9ce8340794a1cd0f3790cd2b99ed80
Davi Gregório <davigregorio.a@hotmail.com>
2025-02-24 14:29:06 -0300

    wine cellar new iteration

--- wine-cellar.ex
+++ wine-cellar.ex
@@ -6,43 +6,29 @@ defmodule WineCellar do
       rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
     ]
   end
-
   def filter(cellar, color, opts \\ []) do
-    wine_by_color = Keyword.get_values(cellar, color)
-    case opts do
-      [] ->
-        wine_by_color
-      [year: year_val]
-        -> wine_by_color |> filter_by_year(year_val)
-      [country: country_val]
-        -> wine_by_color |> filter_by_country(country_val)
-      _ ->
-        year = Keyword.get(opts, :year);
-        country = Keyword.get(opts, :country);
-        wine_by_color |> filter_by_year(year) |> filter_by_country(country)
-    end
+    Keyword.get_values(cellar, color)
+    |> filter_by(&filter_by_year/2, opts[:year])
+    |> filter_by(&filter_by_country/2, opts[:country])
   end
 
-  # The functions below do not need to be modified.
+  defp filter_by(wines, _filter_fn, nil), do: wines
+  defp filter_by(wines, filter_fn, value), do: filter_fn.(wines, value)
 
+  # The functions below do not need to be modified.
   defp filter_by_year(wines, year)
   defp filter_by_year([], _year), do: []
-
   defp filter_by_year([{_, year, _} = wine | tail], year) do
     [wine | filter_by_year(tail, year)]
   end
-
   defp filter_by_year([{_, _, _} | tail], year) do
     filter_by_year(tail, year)
   end
-
   defp filter_by_country(wines, country)
   defp filter_by_country([], _country), do: []
-
   defp filter_by_country([{_, _, country} = wine | tail], country) do
     [wine | filter_by_country(tail, country)]
   end
-
   defp filter_by_country([{_, _, _} | tail], country) do
     filter_by_country(tail, country)
   end
